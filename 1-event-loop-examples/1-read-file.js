1a2672ecfe0e0fe57a33a654416b488501269978
Sakshi Gupta <sakshimailara@gmail.com>
2025-09-22 18:07:31 +0530

    event loop

---
+++ 1-event-loop-examples/1-read-file.js
@@ -0,0 +1,18 @@
+const {readFile}=  require('fs')
+const { result } = require('lodash')
+
+console.log('started a first task')
+// CHECK FILE PATH!!!
+
+readFile('./content/first.txt' , 'utf8' , (err, result) =>{
+    if(err){
+        console.log(err)
+        return
+    }
+    console.log(result)
+    console.log('completed first task')
+
+
+})
+
+console.log('starting next task')
