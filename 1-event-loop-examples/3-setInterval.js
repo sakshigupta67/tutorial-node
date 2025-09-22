4ebd1330793b9a98d5de918e420e0adaa24392eb
Sakshi Gupta <sakshimailara@gmail.com>
2025-09-22 21:10:11 +0530

    event loop

---
+++ 1-event-loop-examples/3-setInterval.js
@@ -0,0 +1,10 @@
+setInterval(() => {
+    console.log('hello world')
+} , 2000)
+
+console.log(' i will run first')
+
+// process stay alive unless
+// kill process CONTROL + C
+// unexpected error 
+ 
